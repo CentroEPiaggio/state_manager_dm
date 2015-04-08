e0e889ec55a96e0557c2a46a4e3b4a65646a7cf9
hamalMarino <hamal.marino@gmail.com>
2015-04-08 22:35:12 +0200

    improved test_semantic_to_cartesian to be more realistic as in top-top scenario

--- tests/test_semantic_to_cartesian.cpp
+++ tests/test_semantic_to_cartesian.cpp
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
     data.target_position.position.x = -0.312;
     data.target_position.position.y = 0.545;
     data.target_position.position.z = 0;
+    data.obj_id = 1;
     semantic_to_cartesian_converter state(database);
     std::vector<std::pair<endeffector_id,cartesian_command>> result;
     dual_manipulation_shared::planner_serviceResponse::_path_type path,path1;
@@ -22,21 +23,30 @@ int main(int argc, char *argv[])
     3 2
     3 1
   */  
-    item.grasp_id=7;
+    item.grasp_id=3;
     item.workspace_id=3;
     path.push_back(item);
-    item.grasp_id=5;
+    item.grasp_id=1;
     item.workspace_id=3;
     path.push_back(item);
-    item.grasp_id=5;
+    item.grasp_id=1;
     item.workspace_id=2;
     path.push_back(item);
     item.grasp_id=3;
     item.workspace_id=2;
+    path.push_back(item);
+    item.grasp_id=2;
+    item.workspace_id=2;
+    path.push_back(item);
+    item.grasp_id=2;
+    item.workspace_id=1;
     path.push_back(item);
     item.grasp_id=3;
     item.workspace_id=1;
     path.push_back(item);
+    
+    std::cout << "path: " << path << std::endl;
+    
     std::vector<dual_manipulation_shared::planner_item> a,b; 
     state.convert(result,path,data,a,b);
     for (auto i:result)
