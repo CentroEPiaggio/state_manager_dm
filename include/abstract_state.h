86121a53ccf1d864fb8f55adc1aa67ae5a0c0b10
hamalMarino <hamal.marino@gmail.com>
2015-04-21 12:24:06 +0200

    added a virtual destructor for states

--- include/abstract_state.h
+++ include/abstract_state.h
@@ -12,6 +12,7 @@ private:
 public:
     abstract_state(){
     }
+    virtual ~abstract_state(){}
     virtual bool isComplete()=0;
     virtual void run()=0;
     virtual std::map<property,bool> getResults()=0;
