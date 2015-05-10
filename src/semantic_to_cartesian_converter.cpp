3e49dfd3ef9bcf4c9bed5c1b31aa4283782b99b1
hamalMarino <hamal.marino@gmail.com>
2015-05-10 04:18:48 +0200

    corrected one waypoint and a way in which another was planned

--- src/semantic_to_cartesian_converter.cpp
+++ src/semantic_to_cartesian_converter.cpp
@@ -369,6 +369,7 @@ bool semantic_to_cartesian_converter::checkSingleGrasp(KDL::Frame& World_Object,
         }
         else
         {
+	    // TODO: make this more general: now the two conditions are equivalent at the last_node only because, for a table, Grasp==PostGrasp!!!
             World_Object = World_Centroid_f*(Object.GraspSecondEE.Inverse());
         }
         if(check_ik(current_ee_name,World_Object*Object.PostGraspFirstEE))
@@ -447,7 +448,7 @@ bool semantic_to_cartesian_converter::convert(std::vector< std::pair< endeffecto
             cartesian_command grasp(cartesian_commands::GRASP,1,node.next_grasp_id);
             tf::poseKDLToMsg(World_Object,grasp.cartesian_task);
             result.push_back(std::make_pair(node.next_ee_id,grasp));
-	    cartesian_command move_no_coll_command(cartesian_commands::MOVE_BEST_EFFORT_NO_COLLISION_CHECK, 1, node.next_grasp_id);
+	    cartesian_command move_no_coll_command(cartesian_commands::MOVE_BEST_EFFORT, 1, node.next_grasp_id);
 	    KDL::Frame World_postGraspSecondEE;
 	    World_postGraspSecondEE = World_Object*(Object.PreGraspFirstEE.Inverse())*Object.PostGraspSecondEE;
             tf::poseKDLToMsg(World_postGraspSecondEE,move_no_coll_command.cartesian_task);
@@ -464,7 +465,7 @@ bool semantic_to_cartesian_converter::convert(std::vector< std::pair< endeffecto
             // 3.6) compute a rough position of the place where the change of grasp will happen
             if (!checkSingleGrasp(World_Object,node,data,false,((next_node_it+1) == path.end()),filtered_source_nodes,filtered_target_nodes))
                 return false;
-            KDL::Frame World_PreGraspSecondEE = World_Object*(Object.PreGraspSecondEE.Inverse())*Object.PostGraspFirstEE;
+            KDL::Frame World_PreGraspSecondEE = World_Object*Object.GraspSecondEE*(Object.PreGraspSecondEE.Inverse())*Object.PostGraspFirstEE;
             tf::poseKDLToMsg(World_PreGraspSecondEE,move_command.cartesian_task);
             result.push_back(std::make_pair(node.current_ee_id,move_command)); //move the first
             KDL::Frame World_GraspSecondEE = World_Object*Object.PostGraspFirstEE;
