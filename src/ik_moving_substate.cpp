6dde7d991aa51a94b3ba52e924cca1b59edac0ea
Hamal Marino <hamal.marino@gmail.com>
2016-09-17 13:07:12 +0200

    code linting before refactor

--- src/ik_moving_substate.cpp
+++ src/ik_moving_substate.cpp
@@ -15,21 +15,21 @@ ik_moving_substate::ik_moving_substate(ik_shared_memory& data):data_(data),db_ma
     if( !ros::isInitialized() )
     {
         int argc;
-	char** argv;
-	ros::init( argc, argv, "state_manager", ros::init_options::AnonymousName );
+        char** argv;
+        ros::init( argc, argv, "state_manager", ros::init_options::AnonymousName );
     }
-
+    
     client = n.serviceClient<dual_manipulation_shared::ik_service>("ik_ros_service");
-
+    
     typedef const dual_manipulation_shared::ik_response::ConstPtr& msg_type;
-
+    
     exe_sub = n.subscribe<ik_moving_substate,msg_type>("ik_control/action_done",1,boost::bind(&ik_moving_substate::callback, this, _1, "Execution"));
     grasp_sub = n.subscribe<ik_moving_substate,msg_type>("ik_control/grasp_done",1,boost::bind(&ik_moving_substate::callback, this, _1, "Grasp"));
     ungrasp_sub = n.subscribe<ik_moving_substate,msg_type>("ik_control/ungrasp_done",1,boost::bind(&ik_moving_substate::callback, this, _1, "Ungrasp"));
-
+    
     n.param("dual_manipulation_parameters/parallelize_plan_execute",parallelize_planning,PARALLELIZE_PLANNING);
     ROS_INFO_STREAM(CLASS_NAMESPACE << " : initialized and " << (parallelize_planning?"":"NOT ") << "parallelizing planning and execution!");
-
+    
     reset();
 }
 
@@ -47,10 +47,10 @@ void ik_moving_substate::reset()
 void ik_moving_substate::callback(const dual_manipulation_shared::ik_response::ConstPtr& str, std::string type)
 {
     std::unique_lock<std::mutex> lck(moving_executed_mutex);
-
+    
     // discard refuse messages (from previous sessions)
     if(moving_executed >= 9999)
-      return;
+        return;
     
     ROS_INFO_STREAM(type.c_str() << ": " << str->group_name.c_str() << " " << str->data << " | moving_executed = " << moving_executed);
     if(str->data=="done")
@@ -58,8 +58,8 @@ void ik_moving_substate::callback(const dual_manipulation_shared::ik_response::C
         if (pending_sequence_numbers.count(str->seq))
         {
             moving_executed--;
-	    if(moving_executed == 0)
-	      data_.robot_moving.store(false);
+            if(moving_executed == 0)
+                data_.robot_moving.store(false);
         }
         else
             ROS_WARN_STREAM("There was an error, ik_control (seq. #" << str->seq << ") returned msg.data : " << str->data);
@@ -69,8 +69,8 @@ void ik_moving_substate::callback(const dual_manipulation_shared::ik_response::C
         ROS_WARN_STREAM("There was an error, ik_control (seq. #" << str->seq << ") returned msg.data : " << str->data);
         failed=true;
         initialized=false;
-	data_.robot_moving.store(false);
-	data_.move_failed.store(true && parallelize_planning);
+        data_.robot_moving.store(false);
+        data_.move_failed.store(true && parallelize_planning);
     }
 }
 
@@ -84,16 +84,16 @@ std::map< ik_transition, bool > ik_moving_substate::getResults()
     }
     else if(failed)
     {
-	results[ik_transition::fail]=failed;
+        results[ik_transition::fail]=failed;
     }
     else if(data_.next_plan == data_.cartesian_plan->size())
     {
-	results[ik_transition::done]=(moving_executed==0);
+        results[ik_transition::done]=(moving_executed==0);
     }
     else
     {
-	// I return to planning if I finished moving OR I can parallelize
-	results[ik_transition::plan]=(moving_executed==0 || (parallelize_planning && move_sent));
+        // I return to planning if I finished moving OR I can parallelize
+        results[ik_transition::plan]=(moving_executed==0 || (parallelize_planning && move_sent));
     }
     return results;
 }
@@ -102,7 +102,7 @@ bool ik_moving_substate::isComplete()
 {  
     std::unique_lock<std::mutex> lck(moving_executed_mutex);
     if(data_.next_plan == data_.cartesian_plan->size()+1) moving_executed=0;
-
+    
     // I can return if I executed the movement, I failed, or I sent the movement AND it's not the last one! (this only if I can parallelize, and NOT for grasping WPs..!)
     return (data_.need_replan.load() || moving_executed==0 || failed || (parallelize_planning && move_sent && !grasping && data_.next_plan < data_.cartesian_plan->size()));
 }
@@ -111,19 +111,19 @@ void ik_moving_substate::run()
 {
     if(!initialized)
     {
-	initialized = true;
+        initialized = true;
     }
     if(move_sent) 
     {
-      // sleep 5ms to allow for other stuff to go on
-      usleep(5000);
-      return;
+        // sleep 5ms to allow for other stuff to go on
+        usleep(5000);
+        return;
     }
     {
-    std::unique_lock<std::mutex> lck(moving_executed_mutex);
-    moving_executed = 0;
+        std::unique_lock<std::mutex> lck(moving_executed_mutex);
+        moving_executed = 0;
     }
-//     geometry_msgs::Pose ee_pose;
+    //     geometry_msgs::Pose ee_pose;
     
     int i=-1;
     int move_num=0;
@@ -131,100 +131,100 @@ void ik_moving_substate::run()
     
     if(data_.cartesian_plan->size()==0)
     {
-	ROS_ERROR("Cartesian plan is empty!!");
+        ROS_ERROR("Cartesian plan is empty!!");
         failed=true;
-	return;
+        return;
     }
-
+    
     do
     {
         i++;
         auto item = data_.cartesian_plan->at(data_.next_plan+i);
-// 	ee_pose=item.second.cartesian_task;
-
-	if(commands.is_exec_alone.count(item.second.command))
-	{
-		if(item.second.seq_num == 0)
-		{
-		    ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : a command to be executed alone (" << item.second.command << ") has seq_num==0! This is NOT allowed!");
-		    failed = true;
-		    initialized = false;
-		}
-		if(item.second.command==cartesian_commands::GRASP || item.second.command==cartesian_commands::UNGRASP)
-		{
-			int grasp = (int)item.second.ee_grasp_id;
-			grasp = grasp % OBJ_GRASP_FACTOR;
-			if(!deserialize_ik(srv.request,"object" + std::to_string((int)*data_.obj_id) + "/grasp" + std::to_string(grasp)))
-			{
-			    ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to deserialize object" + std::to_string((int)*data_.obj_id) + "/grasp" + std::to_string(grasp));
-			    failed = true;
-			    initialized = false;
-			}
-			else
-			{
-			    srv.request.attObject.object.id = *data_.object_name;
-			    srv.request.object_db_id = (int)*data_.obj_id;
-			    ee_name = std::get<0>(db_mapper.EndEffectors.at(item.first));
-                
-                // make names coherent with the current urdf
-                // TODO: make this more general
+        //  ee_pose=item.second.cartesian_task;
+        
+        if(commands.is_exec_alone.count(item.second.command))
+        {
+            if(item.second.seq_num == 0)
+            {
+                ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : a command to be executed alone (" << item.second.command << ") has seq_num==0! This is NOT allowed!");
+                failed = true;
+                initialized = false;
+            }
+            if(item.second.command==cartesian_commands::GRASP || item.second.command==cartesian_commands::UNGRASP)
+            {
+                int grasp = (int)item.second.ee_grasp_id;
+                grasp = grasp % OBJ_GRASP_FACTOR;
+                if(!deserialize_ik(srv.request,"object" + std::to_string((int)*data_.obj_id) + "/grasp" + std::to_string(grasp)))
+                {
+                    ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to deserialize object" + std::to_string((int)*data_.obj_id) + "/grasp" + std::to_string(grasp));
+                    failed = true;
+                    initialized = false;
+                }
+                else
+                {
+                    srv.request.attObject.object.id = *data_.object_name;
+                    srv.request.object_db_id = (int)*data_.obj_id;
+                    ee_name = std::get<0>(db_mapper.EndEffectors.at(item.first));
+                    
+                    // make names coherent with the current urdf
+                    // TODO: make this more general
 #if IS_FACTORY>0
-                if(db_mapper.EndEffectors.size() != NUM_EE_IN_VITO)
-                    if(item.first <= NUM_KUKAS)
-                    {
-                        srv.request.attObject.link_name = std::to_string((int)((item.first-1)/2)) + "_" + srv.request.attObject.link_name;
-                        srv.request.attObject.object.header.frame_id = srv.request.attObject.link_name;
-                        for(auto& j:srv.request.grasp_trajectory.joint_names)
-                            j = std::to_string((int)((item.first-1)/2)) + "_" + j;
-                    }
+                    if(db_mapper.EndEffectors.size() != NUM_EE_IN_VITO)
+                        if(item.first <= NUM_KUKAS)
+                        {
+                            srv.request.attObject.link_name = std::to_string((int)((item.first-1)/2)) + "_" + srv.request.attObject.link_name;
+                            srv.request.attObject.object.header.frame_id = srv.request.attObject.link_name;
+                            for(auto& j:srv.request.grasp_trajectory.joint_names)
+                                j = std::to_string((int)((item.first-1)/2)) + "_" + j;
+                        }
 #endif
-			
-			    // change frame of reference of the grasp trajectory to the current object frame
-			    change_frame_to_pose_vector(item.second.cartesian_task,srv.request.ee_pose);
-			    if(item.second.command == cartesian_commands::UNGRASP)
-			    {
-				// invert the order to generate an ungrasp
-				std::reverse(srv.request.ee_pose.begin(),srv.request.ee_pose.end());
-				std::reverse(srv.request.grasp_trajectory.points.begin(),srv.request.grasp_trajectory.points.end());
-			    }
-			    grasping = true;
-			}
-		}
-		
-		// NOTE: this has to be executed ALONE!
-		break;
-	}
-	else
-	{
-	        move_num++;
-		if(!ee_name.empty() && ee_name != std::get<0>(db_mapper.EndEffectors.at(item.first)))
-		  ee_name = "full_robot";
-		else
-		  ee_name = std::get<0>(db_mapper.EndEffectors.at(item.first));
-	}
+                        
+                        // change frame of reference of the grasp trajectory to the current object frame
+                        change_frame_to_pose_vector(item.second.cartesian_task,srv.request.ee_pose);
+                        if(item.second.command == cartesian_commands::UNGRASP)
+                        {
+                            // invert the order to generate an ungrasp
+                            std::reverse(srv.request.ee_pose.begin(),srv.request.ee_pose.end());
+                            std::reverse(srv.request.grasp_trajectory.points.begin(),srv.request.grasp_trajectory.points.end());
+                        }
+                        grasping = true;
+                }
+            }
+            
+            // NOTE: this has to be executed ALONE!
+            break;
+        }
+        else
+        {
+            move_num++;
+            if(!ee_name.empty() && ee_name != std::get<0>(db_mapper.EndEffectors.at(item.first)))
+                ee_name = "full_robot";
+            else
+                ee_name = std::get<0>(db_mapper.EndEffectors.at(item.first));
+        }
     }
     while((data_.cartesian_plan->at(data_.next_plan+i).second.seq_num==0) && (data_.cartesian_plan->size() > data_.next_plan+i+1));
     
     if(!failed)
     {
         srv.request.command = commands.command[data_.cartesian_plan->at(data_.next_plan+i).second.command];
-	srv.request.ee_name = ee_name;
+        srv.request.ee_name = ee_name;
         std::unique_lock<std::mutex> lck(moving_executed_mutex);
         srv.request.seq=sequence_counter;
-	
-	if(client.call(srv))
-	{
-	    ROS_INFO_STREAM("IK " << data_.cartesian_plan->at(data_.next_plan+i).second.command << " request accepted: (" << (int)srv.response.ack << ") - seq: "<<data_.next_plan);
+        
+        if(client.call(srv))
+        {
+            ROS_INFO_STREAM("IK " << data_.cartesian_plan->at(data_.next_plan+i).second.command << " request accepted: (" << (int)srv.response.ack << ") - seq: "<<data_.next_plan);
             pending_sequence_numbers.insert(sequence_counter);
-	    data_.robot_moving.store(true && parallelize_planning);
-	}
-	else
-	{
-    	    ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to call service ik_ros_service \'" << srv.request.command << "\'");
+            data_.robot_moving.store(true && parallelize_planning);
+        }
+        else
+        {
+            ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to call service ik_ros_service \'" << srv.request.command << "\'");
             initialized=false;
             failed=true;
-	}
-	moving_executed++;
+        }
+        moving_executed++;
         sequence_counter++;
     }
     
@@ -245,7 +245,7 @@ void ik_moving_substate::change_frame_to_pose_vector(geometry_msgs::Pose object_
     tf::poseMsgToKDL(object_pose_msg,object_frame);
     for(int i=0; i<ee_pose.size(); ++i)
     {
-	tf::poseMsgToKDL(ee_pose.at(i),ee_single_frame);
-	tf::poseKDLToMsg(object_frame*ee_single_frame,ee_pose.at(i));
+        tf::poseMsgToKDL(ee_pose.at(i),ee_single_frame);
+        tf::poseKDLToMsg(object_frame*ee_single_frame,ee_pose.at(i));
     }
 }
--- src/ik_planning_substate.cpp
+++ src/ik_planning_substate.cpp
@@ -9,20 +9,20 @@ ik_planning_substate::ik_planning_substate(ik_shared_memory& data):data_(data),d
     if( !ros::isInitialized() )
     {
         int argc=0;
-	char** argv;
-	ros::init( argc, argv, "state_manager", ros::init_options::AnonymousName );
+        char** argv;
+        ros::init( argc, argv, "state_manager", ros::init_options::AnonymousName );
     }
-
+    
     client = n.serviceClient<dual_manipulation_shared::ik_service>("ik_ros_service");
     initialized = false;
     plan_sent=false;
     plan_executed = 9999;
     sequence_counter=0;
-
+    
     typedef const dual_manipulation_shared::ik_response::ConstPtr& msg_type;
     
     plan_sub = n.subscribe<ik_planning_substate,msg_type>("ik_control/planning_done",1,boost::bind(&ik_planning_substate::callback, this, _1, "Just to use a boost::bind"));
-
+    
     reset();
 }
 
@@ -42,7 +42,7 @@ void ik_planning_substate::callback(const dual_manipulation_shared::ik_response:
     
     // discard refuse messages (from previous sessions)
     if(plan_executed >= 9999)
-      return;
+        return;
     
     ROS_INFO_STREAM(str->group_name.c_str()<<" " << str->data << " | plan_executed = " << plan_executed);
     if(str->data=="done")
@@ -68,11 +68,11 @@ std::map< ik_transition, bool > ik_planning_substate::getResults()
     if(data_.need_replan.load())
         results[ik_transition::fail] = true;
     else if(failed || data_.move_failed.load())
-      results[ik_transition::fail] = true;
+        results[ik_transition::fail] = true;
     else
     {
-      results[ik_transition::move]=(plan_executed==0 && plan_sent);
-      results[ik_transition::check_grasp]=checking_grasp;
+        results[ik_transition::move]=(plan_executed==0 && plan_sent);
+        results[ik_transition::check_grasp]=checking_grasp;
     }
     
     return results;
@@ -82,154 +82,154 @@ void ik_planning_substate::run()
 {
     if(!initialized)
     {
-	initialized = true;
+        initialized = true;
     }
-
+    
     if(plan_sent || checking_grasp)
     {
-      // sleep 5ms to allow for other stuff to go on
-      usleep(5000);
-      return;
+        // sleep 5ms to allow for other stuff to go on
+        usleep(5000);
+        return;
     }
-
+    
     plan_executed = 0;
-
+    
     geometry_msgs::Pose ee_pose;
     
     bool is_first_plan = true;
     cartesian_commands last_plan;
     std::string planning_ee_name = "";
-
+    
     srv.request.command = "";
     srv.request.ee_name = "";
     srv.request.ee_pose.clear();
-
+    
     int i=-1;
     
     if(data_.cartesian_plan->size()==0)
     {
-	ROS_ERROR("Cartesian plan is empty!!");
-	return;
+        ROS_ERROR("Cartesian plan is empty!!");
+        return;
     }
     
     do
     {
         i++;
-	auto item = data_.cartesian_plan->at(data_.next_plan+i);
-
-	if(!commands.is_to_be_planned.count(item.second.command))
-	{
-	    if((i != 0) || (data_.cartesian_plan->at(data_.next_plan+i).second.seq_num == 0))
-	    {
-	      ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : I found a command (\'" << item.second.command << "\') not to be planned after " << i << " other(s) to be planned, but those will be IGNORED!!!");
-	      failed = true;
-	      return;
-	    }
-	    
-	    if(commands.is_to_be_checked.count(item.second.command))
-	      checking_grasp = true;
-	    else
-	    {
-	      ROS_INFO_STREAM("Command was not MOVE: returning to ik_moving_substate");
-	      plan_sent = true;
-	    }
-	    return;
-	}
-	
-	if(is_first_plan)
-	{
-	  is_first_plan = false;
-	  last_plan = item.second.command;
-	}
-	else if((commands.can_follow.count(last_plan) == 0) || (commands.can_follow[last_plan].count(item.second.command) == 0))
-	{
-	  ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : trying to serially plan for \'" << last_plan << "\' and \'" << item.second.command << "\', but this is NOT allowed!!!");
-	  failed = true;
-	  return;
-	}
-	
-	// flush old targets
-	if(commands.flushing.count(item.second.command))
-	{
-	  // if the service had to be called (!empty), but I couldn't: ERROR
-	  if(!srv.request.command.empty() && !client.call(srv))
-	  {
-	    ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to call service ik_ros_service " << srv.request.command);
+        auto item = data_.cartesian_plan->at(data_.next_plan+i);
+        
+        if(!commands.is_to_be_planned.count(item.second.command))
+        {
+            if((i != 0) || (data_.cartesian_plan->at(data_.next_plan+i).second.seq_num == 0))
+            {
+                ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : I found a command (\'" << item.second.command << "\') not to be planned after " << i << " other(s) to be planned, but those will be IGNORED!!!");
+                failed = true;
+                return;
+            }
+            
+            if(commands.is_to_be_checked.count(item.second.command))
+                checking_grasp = true;
+            else
+            {
+                ROS_INFO_STREAM("Command was not MOVE: returning to ik_moving_substate");
+                plan_sent = true;
+            }
+            return;
+        }
+        
+        if(is_first_plan)
+        {
+            is_first_plan = false;
+            last_plan = item.second.command;
+        }
+        else if((commands.can_follow.count(last_plan) == 0) || (commands.can_follow[last_plan].count(item.second.command) == 0))
+        {
+            ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : trying to serially plan for \'" << last_plan << "\' and \'" << item.second.command << "\', but this is NOT allowed!!!");
             failed = true;
-	    return;
-	  }
-	  
-	  // clear the service request
-	  srv.request.command = "";
-	  srv.request.ee_name = "";
-	  srv.request.ee_pose.clear();
-	}
-	
-	// add new target at the request
-	srv.request.command = commands.set_target_command.at(item.second.command);
-	
-	std::string &ee_name(srv.request.ee_name);
-	std::string current_ee(std::get<0>(db_mapper.EndEffectors.at(item.first)));
-	//TODO: this code is very specific to our setup!!! make it more general...
-	if(!ee_name.empty() && ee_name != current_ee)
-	{
-	  srv.request.ee_pose.resize(2);
-	  
-	  // if there was a right_hand target, move it to the second position
-	  if(ee_name == "right_hand")
-	    std::swap(srv.request.ee_pose.at(1),srv.request.ee_pose.at(0));
-	  
-	  // insert the new pose in the right place
-	  if(current_ee == "left_hand")
-	    srv.request.ee_pose.at(0) = item.second.cartesian_task;
-	  else if(current_ee == "right_hand")
-	    srv.request.ee_pose.at(1) = item.second.cartesian_task;
-	  else
-	  {
-	    ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : current end-effector \'" << current_ee << "\' is NOT supported!");
-	    failed = true;
-	    return;
-	  }
-	  
-	  ee_name = "both_hands";
-	}
-	else
-	{
-	  ee_name = current_ee;
-	  srv.request.ee_pose.clear();
-	  srv.request.ee_pose.push_back(item.second.cartesian_task);
-	}
-	
-	if(planning_ee_name.empty())
-	  planning_ee_name = ee_name;
-	else if(planning_ee_name != ee_name)
-        // if more than a single group has been considered, always plan for the full robot group
-	  planning_ee_name = "full_robot";
-	
-	// flush newly set target
-	if(commands.flushing.count(item.second.command))
-	{
-	  if(!client.call(srv))
-	  {
-	    ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to call service ik_ros_service " << srv.request.command);
-            failed = true;
-	    return;
-	  }
-	  
-	  // clear the service request
-	  srv.request.command = "";
-	  srv.request.ee_name = "";
-	  srv.request.ee_pose.clear();
-	}
-
+            return;
+        }
+        
+        // flush old targets
+        if(commands.flushing.count(item.second.command))
+        {
+            // if the service had to be called (!empty), but I couldn't: ERROR
+            if(!srv.request.command.empty() && !client.call(srv))
+            {
+                ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to call service ik_ros_service " << srv.request.command);
+                failed = true;
+                return;
+            }
+            
+            // clear the service request
+            srv.request.command = "";
+            srv.request.ee_name = "";
+            srv.request.ee_pose.clear();
+        }
+        
+        // add new target at the request
+        srv.request.command = commands.set_target_command.at(item.second.command);
+        
+        std::string &ee_name(srv.request.ee_name);
+        std::string current_ee(std::get<0>(db_mapper.EndEffectors.at(item.first)));
+        //TODO: this code is very specific to our setup!!! make it more general...
+        if(!ee_name.empty() && ee_name != current_ee)
+        {
+            srv.request.ee_pose.resize(2);
+            
+            // if there was a right_hand target, move it to the second position
+            if(ee_name == "right_hand")
+                std::swap(srv.request.ee_pose.at(1),srv.request.ee_pose.at(0));
+            
+            // insert the new pose in the right place
+            if(current_ee == "left_hand")
+                srv.request.ee_pose.at(0) = item.second.cartesian_task;
+            else if(current_ee == "right_hand")
+                srv.request.ee_pose.at(1) = item.second.cartesian_task;
+            else
+            {
+                ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : current end-effector \'" << current_ee << "\' is NOT supported!");
+                failed = true;
+                return;
+            }
+            
+            ee_name = "both_hands";
+        }
+        else
+        {
+            ee_name = current_ee;
+            srv.request.ee_pose.clear();
+            srv.request.ee_pose.push_back(item.second.cartesian_task);
+        }
+        
+        if(planning_ee_name.empty())
+            planning_ee_name = ee_name;
+        else if(planning_ee_name != ee_name)
+            // if more than a single group has been considered, always plan for the full robot group
+            planning_ee_name = "full_robot";
+        
+        // flush newly set target
+        if(commands.flushing.count(item.second.command))
+        {
+            if(!client.call(srv))
+            {
+                ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to call service ik_ros_service " << srv.request.command);
+                failed = true;
+                return;
+            }
+            
+            // clear the service request
+            srv.request.command = "";
+            srv.request.ee_name = "";
+            srv.request.ee_pose.clear();
+        }
+        
     } while((data_.cartesian_plan->at(data_.next_plan+i).second.seq_num==0) && (data_.cartesian_plan->size() > data_.next_plan+i+1));
-
+    
     // if the service had to be called (!empty), but I couldn't: ERROR
     if(!srv.request.command.empty() && !client.call(srv))
     {
-      ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to call service ik_ros_service " << srv.request.command);
-      failed = true;
-      return;
+        ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to call service ik_ros_service " << srv.request.command);
+        failed = true;
+        return;
     }
     
     srv.request.command = commands.plan_command[data_.cartesian_plan->at(data_.next_plan+i).second.command];
@@ -239,19 +239,19 @@ void ik_planning_substate::run()
     plan_executed++;
     sequence_counter++;
     srv.request.seq=sequence_counter;
-
+    
     std::cout << "data_.next_plan+i = " << data_.next_plan+i << std::endl;
     if(client.call(srv))
     {
         pending_sequence_numbers.insert(sequence_counter);
-	ROS_INFO_STREAM("IK Plan Request accepted: (" << (int)srv.response.ack << ") - seq: "<<data_.next_plan);
-	plan_sent = true;
+        ROS_INFO_STREAM("IK Plan Request accepted: (" << (int)srv.response.ack << ") - seq: "<<data_.next_plan);
+        plan_sent = true;
     }
     else
     {
-	    ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to call service ik_ros_service " << srv.request.command);
-            failed=true;
-            initialized=false;
+        ROS_ERROR_STREAM(CLASS_NAMESPACE << __func__ << " : failed to call service ik_ros_service " << srv.request.command);
+        failed=true;
+        initialized=false;
     }
 }
 
